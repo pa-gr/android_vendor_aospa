280e57dfda80983a7f634e0ed967145f275f6b60
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-10-28 20:17:50 +0530

    lisa: Enable R8 optimizations for SystemUI & system_server
    Change-Id: I9b62d5919a6d04311a9bf844bdbaaa853305fff9

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -95,3 +95,7 @@ SKIP_BOOT_JARS_CHECK := true
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Enable whole-program R8 Java optimizations for SystemUI and system_server
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
